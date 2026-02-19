8aed82998f004e5626841468f42ddf11a620c7fc
Maximilian Gruber <gruber.max2005@gmail.com>
2026-02-19 13:09:16 +0100

    added cors middleware for chrome debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 from .database import engine, Base
 from .routers import auth, site_config, route, employee
 import app.models
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FastAPI + Postgres (Docker Safe Setup)")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(site_config.router)
 app.include_router(route.router)
