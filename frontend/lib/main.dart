9af3cd3697594434eb2e75a01873221391b838c0
Maximilian Gruber <gruber.max2005@gmail.com>
2026-03-16 16:50:33 +0100

    onrender missing in pipeline

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'l10n/app_localizations.dart';
