ec89307fe4dfc0ef06e10dc274e423c595f3589f
Maximilian Gruber <gruber.max2005@gmail.com>
2026-03-16 16:34:51 +0100

    update after kts error

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -12,7 +12,6 @@ android {
 
     signingConfigs {
         create("release") {
-            // System.getenv zieht die Secrets aus der GitHub Action Umgebung
             val keystorePath = System.getenv("ANDROID_KEYSTORE_PATH") ?: "keystore.jks"
             val keystorePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
             val keyAliasName = System.getenv("ANDROID_KEY_ALIAS")
@@ -45,10 +44,8 @@ android {
 
     buildTypes {
         release {
-            // Hier wird jetzt die oben definierte release-Konfiguration verwendet
             signingConfig = signingConfigs.getByName("release")
             
-            // In Kotlin (.kts) heißen die Properties etwas anders:
             isMinifyEnabled = false
             isShrinkResources = false
             
